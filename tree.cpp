88558a2798be268a6d15e371c24fcc1b2271dd42
Eric Kim <35583358+tundrabun@users.noreply.github.com>
2020-06-01 04:25:47 -0700

    Add files via upload

--- tree.cpp
+++ tree.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <time.h>
 
-#define NODESPACE 4
+#define ORDER 5
 
 struct node {
-    std::vector<int> data;
-    std::vector<node*> pointers;
+    int* data;
+    node** pointers;
     
     bool leaf;
     int num;
@@ -14,48 +16,50 @@ struct node {
 
 node* createTree(){
     newnode = new node;
-    newnode->data = std::vector<int>(4);
-    newnode->pointers = std::vector<node*>(5);
+    newnode->data = new int[ORDER];
+    newnode->pointers = new node*[ORDER + 1];
     newnode->leaf = true;
     newnode->num = 0;
 
-    for(int i = 0; i < NODESPACE; i++)
+    for(int i = 0; i < ORDER+1; i++)
         newnode->pointers[i] = NULL;
 
     return newnode;
 }
 
+
 int split(node* n, int i) {
     int j, middle;
-    node *new1, *new3, *child;
+    node *left, *right, *child;
 
-    new3 = createTree();
-    //new3->leaf = true;
+    right = createTree();
+    right->leaf = true;
 
     if(i == -1) {
         middle = n->data[2];
+        n->data[2] = 0;
         n->num -= 1;
 
-        new1 = createTree();
-        new1->leaf = false;
+        left = createTree();
+        left->leaf = false;
         n->leaf = true;
 
-        for(int j = 3; j < NODESPACE; j++) {
-            new3->data[j-3] = n->data[j];
-            new3->pointers[j-3] = n->pointers[j];
-            new3->num += 1;
+        for(int j = 3; j < ORDER; j++) {
+            right->data[j-3] = n->data[j];
+            right->pointers[j-3] = n->pointers[j];
+            right->num += 1;
 
             n->data[j] = 0;
             n->num -= 1;
         }
-        for(int j = 0; j < NODESPACE+1; j++) 
+        for(int j = 0; j < ORDER+1; j++) 
             n->pointers[j] = NULL;
 
-        new1->data[0] = middle;
-        new1->pointers[new1->num] = n;
-        new1->pointers[new1->num + 1] = new3;
-        new1->num += 1;
-        root = new1;
+        left->data[0] = middle;
+        left->pointers[left->num] = n;
+        left->pointers[left->num + 1] = right;
+        left->num += 1;
+        root = left;
     }
     else {
         child = n->pointers[i];
@@ -63,23 +67,27 @@ int split(node* n, int i) {
         child->data[2] = 0;
         child->num -= 1;
 
-        for(int j = 3; j < NODESPACE; j++) {
-            new3->data[j-3] = child->data[j];
-            new3->num += 1;
+        for(int j = 3; j < ORDER; j++) {
+            right->data[j-3] = child->data[j];
+            right->num += 1;
 
             child->data[j] = 0;
             child->num -= 1;
         }
-        n->pointers[i+1] = child;
-        n->pointers[i+1] = new3;
+        std::cout << "t1: " << child->data[0] << " " << child->data[1] << std::endl <<"t2: " << right->data[0]<< " " << right->data[1] << std::endl;
+        
+        n->pointers[i+2] = child;
+        n->pointers[i+2] = right;
+        
     }
 
     return middle;
 }
 
-void sortTree( std::vector<int> arr, int n ) {
+
+void sortTree( int* arr, int n ) {
     for(int i = 0; i<n; i++) {
-        for(int j = i; j<n; j++){
+        for(int j = i; j<=n; j++){
             int temp;
             if( arr[i] > arr[j]) {
                 temp = arr[i];
@@ -91,6 +99,7 @@ void sortTree( std::vector<int> arr, int n ) {
     return;
 }
 
+
 void insert( int i ) {
     int temp;
     int j;
@@ -101,7 +110,7 @@ void insert( int i ) {
         x = root;
     } 
     else {
-        if(x->leaf && x->num == NODESPACE) {
+        if(x->leaf && x->num == ORDER) {
             temp = split(x, -1);
             x = root;
 
@@ -126,7 +135,7 @@ void insert( int i ) {
                     else continue;
                 }
 
-                if( (x->pointers[j])->num == NODESPACE) {
+                if( (x->pointers[j])->num == ORDER) {
                     temp = split(x,j);
                     x->data[x->num] = temp;
                     x->num += 1;
@@ -143,12 +152,19 @@ void insert( int i ) {
     return;
 }
 
+
+
 void printTree(node* n) {
-    std::cout << std::endl;
+    std::cout<<std::endl;
     int count;
+
+    if(!n) return;
+
     for( count = 0; count < n->num; count++) {
-        if(n->leaf == false) { printTree(n->pointers[count]);}
-        std::cout << " " << n->data[count];
+        if(n->leaf == false)
+            printTree(n->pointers[count]);
+
+        std::cout << n->data[count] << " ";
     }
     if( n->leaf == false) { printTree(n->pointers[count]);}
     std::cout << std::endl;
@@ -176,7 +192,17 @@ node* search( int i ) {
 }
 
 int main() {
-    std::cout << "Hello World" << std::endl;
 
+    
+    srand( time(NULL) );
+    for(int i = 0; i < 50; i++) {
+        int temp = rand()%100 + 1;
+        std::cout << "Inserting number " << i+1 << " which is " << temp << std::endl;
+        insert(temp);
+        printTree(root);
+    }
+
+    std::cout << "Printing Tree" << std::endl;
+    std::cout << "test:" << root->pointers[0]->data[0] << std::endl;
     printTree(root);
 }
